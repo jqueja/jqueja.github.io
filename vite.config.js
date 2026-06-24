70b29084c79450199ef6ab309ac6192ee8a26292
Josh Queja <jqueja@calpoly.edu>
2026-06-23 23:03:57 -0700

    making a push

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // base: '/' is correct for a user GitHub Pages site (jqueja.github.io)
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-})
+  base: "/",
+});
